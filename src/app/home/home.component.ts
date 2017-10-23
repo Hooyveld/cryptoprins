b946e85aa735de5f75d7236393b3a2aa420bd59d
Bob Hooyveld <bob.hooyveld@chunk.nl>
2017-10-23 18:41:19 +0200

    Loop over usps

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cardUsps =
+  [
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "link",
+      "description": "Link CryptoPrins to your exchange."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "link",
+      "description": "Link CryptoPrins to your exchange."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "link",
+      "description": "Link CryptoPrins to your exchange."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "link",
+      "description": "Link CryptoPrins to your exchange."
+    }
+  ];
+
+  listUsps =
+  [
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    },
+    {
+      "icon": "/assets/images/usps/analytics.svg",
+      "title": "SECURE STORAGE",
+      "description": "You are keeping the controle over your own exchange and your information is always safely encrypted."
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
